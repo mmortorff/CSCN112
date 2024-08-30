5e2ec6d3d6403e64ab63c1064f33261d2257b2be
mmortorff <mmortorff@liberty.edu>
2024-08-30 11:27:40 -0400

    Participation Lab 1 Template Upload
    This includes all of the template files for participation lab #1

---
+++ CSCN112_PLab1_Template/CSCN112_PLab1_Template/Main.cpp
@@ -0,0 +1,39 @@
+// CSCN 112 Participation Lab #1
+// Learning how to create class objects, header files, and implementation files 
+
+/*
+	INSTRUCTIONS
+
+	Learning Objectives
+	 - Practice proper programming form
+	 - Demonstrate the ability to work in a group
+	 - Learn how to successfully create a class using header and implementation files
+	 - Implement class objects and use them in a program successfully
+
+	 Overview: Your team is starting up a new bakery (you may be creative with your bakery name). You need to create a program that will automate
+	   the ordering system, allowing your patrons to purchase various items from the menu before printing out an overall total. This system will
+	   be created through several labs throughout the semester, but your initial goal is to be able to create menu items. Use the following steps
+	   to guide your process:
+
+	 1.)  Create your header and implementation files for a class named menuItem
+	 2.)  The menuItem class should be used to store the name of the item, the price of the item, and how many of that specific item has been ordered
+	 3.)  Your class should include appropriate functions - accessor and mutator functions and a print function
+	 4.)  In main, confirm that you can create an object of the menuItem class, then create an array of menuItem objects called menu
+	 5.)  Next, you need to be able to bring in the various items that your bakery is going to sell (using a text file and function(s))
+			- Hint: Start by creating a function that can bring in a SINGLE item from a file, then expand to bring in more than one item
+	 6.)  Create a function that prints off the menu
+
+*/
+
+// Include Statements 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "CSCN 112 - Fall 2024 - Participation Lab #1" << endl << endl;
+
+	system("pause");
+	return 0;
+}
